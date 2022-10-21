3706b6a5349bda5aa51252eac5f3666e12f5a259
jsalvinetti <salvinetti@yahoo.com>
2022-10-20 21:03:33 -0300

    Validación Form alta cliente

--- validar_formulario.js
+++ validar_formulario.js
@@ -3,25 +3,10 @@ var apellido = document.getElementById('apellido');
 var dni = document.getElementById('dni');
 var usuario = document.getElementById('user');
 var clave = document.getElementById('password');
+var mail = document.getElementById('mail');
 
 var error = document.getElementById('error');
 error.style.color = 'red';
-
-//function enviarFormulario(){
-
-//    console.log('Enviando formulario...');
-
-//    var mensajesError = [];
-
-//    if (nombre.value == null || nombre.value === ''){
-//        mensajesError.push('Ingresa tu nombre');
-//    }
-
-//      error.innerHTML = mensajesError.join(',');
-
- //   return false;
-
-//} 
 
 
 var form = document.getElementById('formulario');
@@ -32,23 +17,27 @@ var form = document.getElementById('formulario');
     var mensajesError = [];
 
     if (nombre.value === null || nombre.value === ''){
-        mensajesError.push('Ingresa tu nombre');
+        mensajesError.push('Ingresá tu Nombre');
     }
 
     if (apellido.value === null || apellido.value === ''){
-        mensajesError.push('Ingresa tu apellido');
+        mensajesError.push('Ingresá tu Apellido');
     }
 
     if (dni.value === null || dni.value === ''){
-        mensajesError.push('Ingresa tu DNI');
+        mensajesError.push('Ingresá tu DNI');
     }
 
     if (usuario.value === null || usuario.value === ''){
-        mensajesError.push('Ingresa tu Usuario');
+        mensajesError.push('Ingresá tu Usuario');
     }
 
     if (clave.value === null || clave.value === ''){
-        mensajesError.push('Ingresa tu Clave');
+        mensajesError.push('Ingresá tu Clave');
+    }
+
+    if (mail.value === null || mail.value === ''){
+        mensajesError.push('Ingresá tu Mail');
     }
 
 
